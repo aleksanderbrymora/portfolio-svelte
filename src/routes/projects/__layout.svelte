d5cc6ac7ef77cd3c630857d885d6fb81baaa2b74
Aleksander Brymora <aleksander.australia@gmail.com>
2022-01-19 17:23:07 +0100

    responsive

--- src/routes/projects/__layout.svelte
+++ src/routes/projects/__layout.svelte
@@ -36,8 +36,8 @@
 		{#each paths as p}
 			<a
 				class={clsx(
-					'flex gap-3 items-center justify-between px-3 sm:px-5 py-2',
-					$page.url.pathname === p.path && 'text-zinc-800 bg-white rounded-xl '
+					'flex gap-3 items-center justify-between px-3 sm:px-5 py-2 w-full',
+					$page.url.pathname === p.path && 'text-zinc-800 bg-white rounded-xl'
 				)}
 				href={p.path}
 				aria-selected={$page.url.pathname === p.path}
